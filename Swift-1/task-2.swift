523ae9290af9603efdc0171d9cb36fadb580f730
your github name <your github email>
2025-05-26 00:51:29 +0400

    added task 2

--- Swift-1/task-2.swift
+++ Swift-1/task-2.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+public func isBalancedParentheses(input: String) -> Bool {
+    let len = input.count
+    var i = 0
+    var j = len - 1
+    if input.filter { $0 == "(" }.count !=  input.filter { $0 == ")" }.count {
+        return false
+    }
+    while  i <= j {
+        let leftIndex = input.index(input.startIndex, offsetBy: i)
+        let rightIndex = input.index(input.startIndex, offsetBy: j)
+        
+        if input[leftIndex] == "(" {
+            while i < j && input[rightIndex] != ")" {
+                j -= 1
+                if j < i {
+                    return false
+                }
+            }
+            if input[rightIndex] != ")" {
+                return false
+            }
+            i += 1
+            j -= 1
+        } else if input[leftIndex] == ")" {
+            return false
+        } else {
+            i += 1
+        }
+    }
+    return true
+}
