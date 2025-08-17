73aa6684f92ad3aa287d14ddae79e44103da5148
your github name <your github email>
2025-08-17 15:22:41 +0400

    deleted .DS_Store

--- SwiftUI/State Management/Task_7/Task_7/ContentView.swift
+++ SwiftUI/State Management/Task_7/Task_7/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class CounterModel {
+    var counter: Int = 0
+}
 
 struct ContentView: View {
+    @State private var counter = CounterModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CounterA(model: counter)
+            CounterB(model: counter)
         }
         .padding()
+    }
+}
+
+struct CounterA: View {
+    var model: CounterModel
+    
+    var body: some View {
+        @Bindable var model = model
+        VStack {
+            Text("CounterA: \(model.counter)")
+            Button("+1 from A") {
+                model.counter += 1
+            }
+            .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+struct CounterB: View {
+    var model: CounterModel
+    
+    var body: some View {
+        @Bindable var model = model
+        VStack {
+            Text("CounterB: \(model.counter)")
+            Button("+1 from B") {
+                model.counter += 1
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
